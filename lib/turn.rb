cd682d487a7ab3c402d7a1d46b1fd2f44cedf6ac
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-08 18:31:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,9 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-    while !valid_move?(board, index) == true
-      #move(board, index, value = "X")
-    #else
-      #puts display_board(board)
+    if valid_move?(board, index) == true
+      move(board, index, value = "X")
+    else
       puts turn(board)
   end
 end
