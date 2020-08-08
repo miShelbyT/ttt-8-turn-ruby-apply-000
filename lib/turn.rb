e8bb0da504cfc9ebeae4b957ed8b4d9989bb61d4
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-08 18:38:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,7 @@ def turn(board)
   index=input_to_index(input)
     if valid_move?(board, index) == true
       move(board, index, value = "X")
+      puts display_board(board)
     else
       puts turn(board)
       puts display_board(board)
