9d741421e48f6089373698c3589442c283b79a8e
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-08 18:21:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
   index=input_to_index(input)
-    if valid_move?(board, index) == true
-      move(board, index, value = "X")
+    if !valid_move?(board, index) == true
+      #move(board, index, value = "X")
     else
-      puts display_board(board)
+      #puts display_board(board)
       puts "Invalid move. Please enter 1-9:"
   end
 end
